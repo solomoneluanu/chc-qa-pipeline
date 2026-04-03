4117d67c447979d4090ad3b6a5e82bf1453ec061
solomoneluanu <solomonkassahun799@gmail.com>
2026-04-03 00:38:39 -0700

    Update run_pipeline.py

--- scripts/run_pipeline.py
+++ scripts/run_pipeline.py
@@ -17,7 +17,7 @@ def main():
     parser = argparse.ArgumentParser(description="Run CHC-QA pipeline")
     parser.add_argument(
         "--input",
-        default="data/input-data/Evalution01.xlsx",
+        default="data/input-data/evalution01.xlsx",
         help="Path to input Excel file"
     )
     args = parser.parse_args()
